1c2b07f81f3286b7672411ef36b3486815c60682
Matheus Macedo <101157827+mattzu22@users.noreply.github.com>
2025-11-04 20:34:26 -0300

    fix

--- src/components/figma/ImageWithFallback.tsx
+++ src/components/figma/ImageWithFallback.tsx
@@ -14,17 +14,15 @@ export function ImageWithFallback(props: React.ImgHTMLAttributes<HTMLImageElemen
   const { src, alt, style, className, ...rest } = props
 
   return didError ? (
-    // <div
-    //   className={`inline-block bg-gray-100 text-center align-middle ${className ?? ''}`}
-    //   style={style}
-    // >
-    //   <div className="flex items-center justify-center w-full h-full">
-    //     <img src={ERROR_IMG_SRC} alt="Error loading image" {...rest} data-original-url={src} />
-    //   </div>
-    // </div>
-    <SkeletonLoader />
+    <div
+      className={`inline-block bg-gray-100 text-center align-middle ${className ?? ''}`}
+      style={style}
+    >
+      <div className="flex items-center justify-center w-full h-full">
+        <img src={ERROR_IMG_SRC} alt="Error loading image" {...rest} data-original-url={src} />
+      </div>
+    </div>
   ) : (
-    // <img src={src} alt={alt} className={className} style={style} {...rest} onError={handleError} />
-    <SkeletonLoader />
+    <img src={src} alt={alt} className={className} style={style} {...rest} onError={handleError} />
   )
 }
