10079a030a33b62f392b39af1843e8c58b8919d5
Matheus Macedo <101157827+mattzu22@users.noreply.github.com>
2025-12-26 19:45:05 -0300

    refactor: refactor particles effect

--- src/page/Homepage/components/ui/particlesEffect.tsx
+++ src/page/Homepage/components/ui/particlesEffect.tsx
@@ -3,14 +3,20 @@ export function ParticlesEffect() {
     return (
         <>
             <div className="particle absolute w-[3px] h-[3px] bg-[#D4AF37] rounded-full opacity-0 animate-float-particle z-5" style={{ left: '20%', animationDelay: '0s' }}></div>
+            <div className="particle absolute w-[4px] h-[4px] bg-[#D4AF37] rounded-full opacity-0 animate-float-particle z-5" style={{ left: '25%', animationDelay: '1s', animationDuration: '10s' }}></div>
+            <div className="particle absolute w-[2px] h-[2px] bg-[#D4AF37] rounded-full opacity-0 animate-float-particle z-5" style={{ left: '30%', animationDelay: '1s' }}></div>
             <div className="particle absolute w-[3px] h-[3px] bg-[#D4AF37] rounded-full opacity-0 animate-float-particle z-5" style={{ left: '40%', animationDelay: '2s' }}></div>
-            <div className="particle absolute w-[3px] h-[3px] bg-[#D4AF37] rounded-full opacity-0 animate-float-particle z-5" style={{ left: '60%', animationDelay: '4s' }}></div>
-            <div className="particle absolute w-[3px] h-[3px] bg-[#D4AF37] rounded-full opacity-0 animate-float-particle z-5" style={{ left: '80%', animationDelay: '6s' }}></div>
-            <div className="particle absolute w-[3px] h-[3px] bg-[#D4AF37] rounded-full opacity-0 animate-float-particle z-5" style={{ left: '30%', animationDelay: '1s' }}></div>
+            <div className="particle absolute w-[2px] h-[2px] bg-[#D4AF37] rounded-full opacity-0 animate-float-particle z-5" style={{ left: '45%', animationDelay: '5s', animationDuration: '7s' }}></div>
             <div className="particle absolute w-[3px] h-[3px] bg-[#D4AF37] rounded-full opacity-0 animate-float-particle z-5" style={{ left: '50%', animationDelay: '3s' }}></div>
+            <div className="particle absolute w-[4px] h-[4px] bg-[#D4AF37] rounded-full opacity-0 animate-float-particle z-5" style={{ left: '55%', animationDelay: '6s' }}></div>
+            <div className="particle absolute w-[3px] h-[3px] bg-[#D4AF37] rounded-full opacity-0 animate-float-particle z-5" style={{ left: '60%', animationDelay: '4s' }}></div>
+            <div className="particle absolute w-[2px] h-[2px] bg-[#D4AF37] rounded-full opacity-0 animate-float-particle z-5" style={{ left: '65%', animationDelay: '2s', animationDuration: '9s' }}></div>
             <div className="particle absolute w-[3px] h-[3px] bg-[#D4AF37] rounded-full opacity-0 animate-float-particle z-5" style={{ left: '70%', animationDelay: '5s' }}></div>
+            <div className="particle absolute w-[4px] h-[4px] bg-[#D4AF37] rounded-full opacity-0 animate-float-particle z-5" style={{ left: '75%', animationDelay: '8s' }}></div>
+            <div className="particle absolute w-[3px] h-[3px] bg-[#D4AF37] rounded-full opacity-0 animate-float-particle z-5" style={{ left: '80%', animationDelay: '6s' }}></div>
+            <div className="particle absolute w-[2px] h-[2px] bg-[#D4AF37] rounded-full opacity-0 animate-float-particle z-5" style={{ left: '85%', animationDelay: '3s', animationDuration: '11s' }}></div>
             <div className="particle absolute w-[3px] h-[3px] bg-[#D4AF37] rounded-full opacity-0 animate-float-particle z-5" style={{ left: '90%', animationDelay: '7s' }}></div>
+            <div className="particle absolute w-[4px] h-[4px] bg-[#D4AF37] rounded-full opacity-0 animate-float-particle z-5" style={{ left: '95%', animationDelay: '9s', animationDuration: '6s' }}></div>
         </>
     )
 }
-
